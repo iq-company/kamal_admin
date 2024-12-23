7d27a8c8e255ae8540663472c4c6c18bda33af44
Philipp Gruener <philipp.gruener@royal-software.de>
2024-12-23 16:51:51 +0100

    draft: wip iframe integration

--- kamal_admin/kamal_admin/doctype/k_cluster/k_cluster.js
+++ kamal_admin/kamal_admin/doctype/k_cluster/k_cluster.js
@@ -1,8 +1,20 @@
 // Copyright (c) 2024, IQ GmbH and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("K Cluster", {
-// 	refresh(frm) {
+frappe.ui.form.on("K Cluster", {
+	refresh(frm) {
 
-// 	},
-// });
+		this.resetNodes(frm);
+	},
+
+	resetNodes(frm) {
+		// const nodesParent = frm.page.parent.querySelector(".frappe-control[data-fieldtype=HTML][data-fieldname=nodes_area]");
+
+		// frappe.router.make_url("desk#List/K Node")
+		// 	.then((url) => {
+		// 	nodesParent.innerHTML = `<iframe src='${url}' style='width: 100%; height: 100%; border: none;'></iframe>`;
+		// 	});
+		// nodesParent.innerHTML = "<iframe src='/desk#List/K Node' style='width: 100%; height: 100%; border: none;'></iframe>";
+	}
+});
+
